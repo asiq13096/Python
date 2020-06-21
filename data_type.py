2e3f017efeb5de3ae3913262ff250162a4448b6a
Asequr Rahman <asiq13096@gmail.com>
2020-06-21 21:58:21 +0600

    2

---
+++ data_type.py
@@ -0,0 +1,6 @@
+age = 32
+print(type(age))
+height = 5.4
+print(type(height))
+line = """Hello Asiq"""
+print(type(line))
---
+++ type_cast.py
@@ -0,0 +1,3 @@
+birth_year = input("What is your birth year: ")
+age = 2020 - int(birth_year)
+print("You are " + str(age) + " years old!!!")
